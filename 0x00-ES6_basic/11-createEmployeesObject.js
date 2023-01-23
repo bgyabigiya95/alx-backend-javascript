d95b795ced913f950d9b295d515917d8006a5ee7
root <bgyabigiya95@gmail.com>
2023-01-23 10:34:46 +0000

    commit

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,9 +1,7 @@
-export default function createIteratorObject(report) {
-  return (function* _() {
-    for (const department of Object.values(report.allEmployees)) {
-      for (const employee of department) {
-        yield employee;
-      }
-    }
-  }());
+export default function createEmployeesObject(departmentName, employees) {
+  return {
+    [departmentName]: [
+      ...employees,
+    ],
+  };
 }
